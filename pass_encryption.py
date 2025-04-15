c4c34e33204014a16ffd184cd3701c793956e801
DoktorNiczego <neron1051@gmail.com>
2025-04-15 11:45:28 +0200

    key generation

--- pass_encryption.py
+++ pass_encryption.py
@@ -1,6 +1,7 @@
 import string
 import random
 import datetime
+from cryptography.fernet import Fernet
 
 
 def pass_gen(size, chars=string.ascii_letters + string.digits + string.punctuation):
@@ -16,7 +17,7 @@ def main():
 
     while True: 
         try:   
-            print("Hey, I can generate password. Enter password length (must be at least 5 characters):\n")
+            print("Hey, I can generate password. Enter password length (must be at least 5 characters):")
             pass_length = int(input())
             if pass_length > 4:
                 password = pass_gen(pass_length)
@@ -28,7 +29,7 @@ def main():
 
 
     try:
-        print("Which service or platform is this password for? (Discord, Gmail etc.):\n")
+        print("Which service or platform is this password for? (Discord, Gmail etc.):")
         service_name = input()
         #possible char limit to add.
     except ValueError:
@@ -37,13 +38,17 @@ def main():
 
     while True:
         try:
-            print("Do you want to save your password? y/n \n")
+            print("Do you want to encrypt and save your password? y/n")
             save_choice = input()
             if save_choice == "n":
                 return
             elif save_choice == "y":
+                key = Fernet.generate_key()
+                cipher = Fernet(key)
+                encrypted_password = cipher.encrypt(password.encode())
+
                 with open("my_passwords.txt", "a") as file:
-                    file.write(f"{current_time} | {service_name} | {password}\n")
+                    file.write(f"{current_time} | {service_name} | {encrypted_password}\n")
                 break
             else:
                 print("Sorry, wrong answer.")
